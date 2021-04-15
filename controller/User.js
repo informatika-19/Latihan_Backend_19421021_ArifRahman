a04a6a2f7feca3e1fc0589d31cf47d0355f5e2b6
arif2907 <arif.19421021@student.ubl.ac.id>
2021-04-15 11:41:33 +0700

    Pertemuan_7

--- controller/User.js
+++ controller/User.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt')
 exports.register = (data) =>
 new Promise((resolve, reject) =>{
 //console.log(data)
+//ini coding untuk mencari 1 data
 userModel.findOne({
     UserName: data.UserName
 }).then(adauser => {
@@ -15,6 +16,7 @@ userModel.findOne({
     }else {
         bcrypt.hash(data.Password, 10, (err, hash) =>{
             data.Password = hash
+            // Untuk Menginput (INSERT Data)
             userModel.create(data)
             .then(() =>{
             //console.log('berhasil insert')
@@ -34,4 +36,33 @@ userModel.findOne({
     }
 })
 
+})
+exports.login = (data) =>
+new Promise((resolve, reject) =>{
+    try {
+        userModel.findOne({
+            UserName: data.UserName
+        }).then(user => {
+            if (user) {
+                if (bcrypt.compareSync(data.Password, user.Password)) {
+                    resolve({
+                        status: true,
+                        pesan: 'BERHASIL LOGIN'
+                    })
+                } else {
+                    reject({
+                        status: false,
+                        pesan: 'PASSWORD TIDAK SESUAI'
+                    })
+                }
+            } else {
+                reject({
+                    status: false,
+                    pesan: 'USERNAME TIDAK TERDAFTAR'
+                })
+            }
+        })
+    } catch (e) {
+        console.log(e)
+    }
 })
