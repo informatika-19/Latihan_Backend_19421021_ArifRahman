c8db7e6d4f3e2328197f97892f95d7b1f85fb371
arif2907 <arif.19421021@student.ubl.ac.id>
2021-04-22 15:37:50 +0700

    Pertemuan 8

--- controller/kegiatan.js
+++ controller/kegiatan.js
@@ -1,4 +1,6 @@
+
 const kegiatanModel = require('../model/kegiatan')
+const objectId = require('mongoose').Types.ObjectId
 
 exports.create = (data) =>
 new Promise((resolve, reject) => {
@@ -10,5 +12,65 @@ new Promise((resolve, reject) => {
     })).catch(() => reject({
         status: false,
         pesan: 'GAGAL INPUT KEGIATAN'
+    }))
+})
+
+exports.showAllData = () =>
+new Promise((resolve, reject) => {
+    kegiatanModel.find()
+    .then(result => {
+        resolve({
+            status: true,
+            pesan: 'BERHASIL MENAMPILKAN DATA !',
+            data: result
+        })
+    }).catch(() => reject ({
+        status: false,
+        pesan: 'GAGAL MENAMPILKAN DATA',
+        data: []
+    }))
+})
+
+exports.showDataById = (id) =>
+new Promise((resolve, reject) => {
+    kegiatanModel.findOne({
+        _id: objectId(id)
+    })
+        .then(result => {
+            resolve({
+                status: true,
+                pesan: 'BERHASIL MENAMPILKAN DATA !',
+                data: result
+            })  
+        }).catch(() => reject ({
+            status: false,
+            pesan: 'GAGAL MENAMPILKAN DATA',
+            data: null
+        }))
+})
+
+exports.update = (id, data) => 
+new Promise((resolve, reject) => {
+    kegiatanModel.updateOne({
+        _id: objectId(id)
+    }, data).then(() => resolve({
+        status: true,
+        pesan: 'BERHASIL MENGUBAH DATA'
+    })).catch(() => reject({
+        status: false,
+        pesan: 'GAGAL MENGUBAH DATA'
+    }))
+})
+
+exports.delete = (id) =>
+new Promise ((resolve, reject) => {
+    kegiatanModel.deleteOne({
+        _id : objectId(id)
+    }).then(() => resolve({
+        status: true,
+        pesan: 'BERHASIL MENGHAPUS DATA'
+    })).catch(() => ({
+        status: false,
+        pesan: 'GAGAL MENGHAPUS DATA'
     }))
 })
